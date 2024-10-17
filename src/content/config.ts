0a666a526e2a528616970be1fc3e47ed7bc46b36
James Best <james.best@breedr.co>
2024-10-17 10:04:03 +0100

    Make all posts the same type

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,20 +13,6 @@ const blogCollection = defineCollection({
     }),
 });
 
-const brilliantBoffinsCollection = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      author: z.string(),
-      tags: z.array(z.string()),
-      description: z.string(),
-      pubDate: z.string().transform((str) => new Date(str)),
-      imgUrl: image(),
-      draft: z.boolean().optional().default(false),
-    }),
-});
-
 export const collections = {
   blog: blogCollection,
-  brilliantBoffins: brilliantBoffinsCollection,
 };
